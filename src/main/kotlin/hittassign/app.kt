001f472688c0d14243a0e0464165ffef270252a8
lostintime <lostintime.dev@gmail.com>
2018-05-22 20:59:40 +0300

    Add app stub

---
+++ src/main/kotlin/hittassign/app.kt
@@ -0,0 +1,19 @@
+package hittassign
+
+import kotlinx.coroutines.experimental.*
+import kotlin.system.exitProcess
+
+suspend fun executeApp(): Int {
+    delay(1000L)
+
+    return 42
+}
+
+fun main(args: Array<String>) {
+    runBlocking {
+        val result = async { executeApp() }
+
+        println("done. ${result.await()}")
+        exitProcess(if (result.await() > 0) 0 else 1)
+    }
+}
